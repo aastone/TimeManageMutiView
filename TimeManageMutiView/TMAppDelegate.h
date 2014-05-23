e5e41ecbb9ea627a4c0febfe840d522946ab2ae7
aastone <stoneiswang@gmail.com>
2014-05-23 15:15:10 +0800

    Initial Commit

---
+++ TimeManageMutiView/TMAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  TMAppDelegate.h
+//  TimeManageMutiView
+//
+//  Created by stone on 5/23/14.
+//  Copyright (c) 2014 stone. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TMAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ TimeManageMutiView/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  TimeManageMutiView
+//
+//  Created by stone on 5/23/14.
+//  Copyright (c) 2014 stone. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "TMAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([TMAppDelegate class]));
+    }
+}
